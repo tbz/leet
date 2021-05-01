2c0fb2a291468db8b7281b453f23e1a668f77e5b
Tobias Baaz <tobias@baaz.nu>
2021-05-01 10:26:48 +0200

    Add working ssr (always english)

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,8 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { hydrate, render } from "react-dom";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./index.css";
 import App from "./App";
-// import * as serviceWorker from "./serviceWorker";
 import { Language, languages } from "./i18n";
 import NotFound from "./NotFound";
 import Clock from "./Clock";
@@ -13,37 +12,40 @@ const props = {
   minute: 37,
 };
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <App {...props} />
-        </Route>
-        <Route
-          sensitive
-          exact
-          path={Object.keys(languages).map((locale) => `/${locale}`)}
-          render={({ location }) => (
-            <App
-              {...props}
-              language={location.pathname.substr(1) as Language}
-            />
-          )}
-        />
-        <Route sensitive exact path="/clock">
-          <Clock {...props} />
-        </Route>
-        <Route path="*">
-          <NotFound />
-        </Route>
-      </Switch>
-    </Router>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+function Site() {
+  return (
+    <React.StrictMode>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <App {...props} />
+          </Route>
+          <Route
+            sensitive
+            exact
+            path={Object.keys(languages).map((locale) => `/${locale}`)}
+            render={({ location }) => (
+              <App
+                {...props}
+                language={location.pathname.substr(1) as Language}
+              />
+            )}
+          />
+          <Route sensitive exact path="/clock">
+            <Clock {...props} />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+      </Router>
+    </React.StrictMode>
+  );
+}
 
-// // If you want your app to work offline and load faster, you can change
-// // unregister() to register() below. Note this comes with some pitfalls.
-// // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
+const rootElement = document.getElementById("root");
+if (rootElement?.hasChildNodes()) {
+  hydrate(<Site />, rootElement);
+} else {
+  render(<Site />, rootElement);
+}
